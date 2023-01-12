dadc2a20a2f408749dd26fc828ccf98f28475439
kelass <kelas2003@gmail.com>
2023-01-12 14:02:50 +0000

    Create some classes

---
+++ Restraunt.Core/BaseObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restraunt.Core
+{
+    public class BaseObject
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
