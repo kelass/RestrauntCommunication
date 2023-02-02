6cd7c7743a0399e84bd23cca07ecedc3534ae549
kelass <69418373+kelass@users.noreply.github.com>
2023-02-02 19:50:59 +0200

    CRM tables and dishes

--- Restraunt.WebUI/wwwroot/js/TableGet.js
+++ Restraunt.WebUI/wwwroot/js/TableGet.js
@@ -15,5 +15,24 @@ async function send() {
 
     }).then(r=>r.json());
 
-    
+    var tables = await response;
+
+    tables.forEach(function (element) {
+        var Nametd = document.createElement('td');
+        var Usertd = document.createElement('td');
+        var tr = document.createElement('tr');
+
+        if (element.user === null) {
+            element.user = "CLEAR";
+        }
+       
+        Nametd.append(element.name);
+        Usertd.append(element.user);
+
+
+        
+        tr.appendChild(Nametd);
+        tr.appendChild(Usertd);
+        document.getElementById('tbody').appendChild(tr)
+    });
 }
